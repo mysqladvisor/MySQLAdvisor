5c7bef9764e67c8aa539bcd4e64b80d51b757b17
zhangqiong6 <zhangqiong6@100tal.com>
2018-11-08 17:45:00 +0800

    替换了新的QQ交流群二维码

---
+++ sqladvisor/CMakeFiles/sqladvisor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/sqladvisor.dir/main.cc.o"
+  "sqladvisor.pdb"
+  "sqladvisor"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/sqladvisor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
